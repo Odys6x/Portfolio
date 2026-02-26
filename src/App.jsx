04f217db62a228d461b26548c03f14217038850a
Odyssey <phoinexw@gmail.com>
2026-02-26 21:40:36 +0800

    2nd push

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import Certifications from './components/Certification'
 import MusicPlayer from './components/MusicPlayer'
+import Experience from './components/Experience'
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
       <MusicPlayer />
       <Hero />
       <About />
+      <Experience /> 
       <Projects />
+      <Certifications />
       <Contact />
     </div>
   )
